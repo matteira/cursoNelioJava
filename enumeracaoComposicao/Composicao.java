aae26c29c37096e3511361d002d0d2a8f9265ed9
Gabriel Mattos <gabrielmattoscosta4@gmail.com>
2022-04-19 12:18:51 -0300

    Treino de enumeração e composição

---
+++ enumeracaoComposicao/Composicao.java
@@ -0,0 +1,7 @@
+package enumeracaoComposicao;
+
+public class Composicao {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ enumeracaoComposicao/Enumeracao.java
@@ -0,0 +1,7 @@
+package enumeracaoComposicao;
+
+public class Enumeracao {
+    public static void main(String[] args) {
+
+    }
+}
